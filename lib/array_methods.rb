1602f8b42ae45ce6e5a9f35f616788bca995371d
Cedric David <cedric.david@btopenworld.com>
2018-09-02 01:06:38 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,17 +7,17 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
-
+  array.first
 end
 
 def using_last(array)
-
+  arry.last
 end
 
 def using_size(array)
-
+  arrau.size
 end
